586d8683d1dfe93295d133b6e8d20029ae03010a
Kyle Barbary <kylebarbary@gmail.com>
2016-10-31 18:32:38 -0700

    snfit install procedure

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # sncosmo-comparison
+
 Compare fit results of sncosmo and snfit
+
+Setup
+=====
+
+
+Download code and JLA data:
+
+```
+./download.sh
+```
+
+Build snfit:
+
+```
+DIR=$(pwd)
+cd src/snfit-2.4.2
+export LIBS="-lgfortran"
+./configure --prefix=$DIR
+make
+make install
+```
+
+Environment variables (add to `.bashrc` or similar):
+
+```
+export LD_LIBRARY_PATH=$DIR/lib:$LD_LIBRARY_PATH
+export SALTPATH=$DIR/snfit_data
+```
+
+Test:
+
+```
+mkdir test
+cd test
+../bin/snfit ../jla_light_curves/lc-03D4ag.list
+```
---
+++ download.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+URL=http://supernovae.in2p3.fr/salt/lib/exe/fetch.php?media=
+VERSION=2.4.2
+
+# download
+mkdir -p download
+wget ${URL}salt2-4_data.tgz -O download/salt2-4_data.tgz
+wget ${URL}snfit-${VERSION}.tar.gz -O download/snfit-${VERSION}.tar.gz
+wget http://supernovae.in2p3.fr/sdss_snls_jla/jla_light_curves.tgz -O download/jla_light_curves.tgz
+
+# unpacking data
+tar xvzf download/salt2-4_data.tgz
+tar xvzf download/jla_light_curves.tgz
+
+# unpack source code
+#mkdir src
+#tar xvzf download/snfit-${VERSION}.tar.gz
+#mv snfit-${VERSION} src/
